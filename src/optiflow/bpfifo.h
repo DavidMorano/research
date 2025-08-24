4e63e7befd65e2669591105c4537995a77502423
David A.D. Morano <david.a.morano@gmail.com>
2025-08-24 00:12:09 -0400

    comments + update

--- src/optiflow/bpfifo.h
+++ src/optiflow/bpfifo.h
@@ -1,52 +1,67 @@
-/* bpfifo */
+/* bpfifo HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 (conformance reviewed) */
+
+/* branch prediction FIFO */
+/* version %I% last-modified %G% */
 
 
+/* revision history:
+
+	= 1998-11-01, David Morano
+	Originally written for Audix Database Processor (DBP) work.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 #ifndef	BPFIFO_INCLUDE
-#define	BPFIFO_INCLUDE		1
+#define	BPFIFO_INCLUDE
 
 
-#include	<sys/types.h>
-
-#include	"localmisc.h"		/* for { uint, ULONG } */
-
+#include	<envstandards.h>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<usyscalls.h>
 
 
 #define	BPFIFO		struct bpfifo_head
-#define	BPFIFO_ENTRY	struct bpfifo_entry
-
-
+#define	BPFIFO_ENT	struct bpfifo_entry
+#define	BPFIFO_MAGIC	0x94732651
 
 
 struct bpfifo_entry {
-	ULONG		in ;
-	ULONG		ia ;
+	ulong		in ;
+	ulong		ia ;
 	uint		row ;
 	uint		outcome ;
 } ;
 
 struct bpfifo_head {
-	unsigned long	magic ;
-	BPFIFO_ENTRY	*table ;
-	int		head, tail ;
+	BPFIFO_ENT	*table ;
+	uint		magic ;
+	int		head ;
+	int		tail ;
 	int		n ;
 } ;
 
+typedef	BPFIFO		bpfifo ;
+typedef	BPFIFO_ENT	bpfifo_ent ;
 
+EXTERNC_begin
 
+extern int	bpfifo_init(BPFIFO *,int) noex ;
+extern int	bpfifo_free(BPFIFO *) noex ;
+extern int	bpfifo_add(BPFIFO *,ulong,ulong,uint,uint) noex ;
+extern int	bpfifo_rem(BPFIFO *,ulong *,ulong *,uint *,uint *) noex ;
+extern int	bpfifo_read(BPFIFO *,ulong *,ulong *,uint *,uint *) noex ;
 
-#if	(! defined(BPFIFO_MASTER)) || (BPFIFO_MASTER == 0)
+EXTERNC_end
 
-extern int	bpfifo_init(BPFIFO *,int) ;
-extern int	bpfifo_free(BPFIFO *) ;
-extern int	bpfifo_add(BPFIFO *,ULONG,ULONG,uint,uint) ;
-extern int	bpfifo_remove(BPFIFO *,ULONG *,ULONG *,uint *,uint *) ;
-extern int	bpfifo_read(BPFIFO *,ULONG *,ULONG *,uint *,uint *) ;
 
 #endif /* BPFIFO */
-
-
-#endif /* BPFIFO */
-
 
 
