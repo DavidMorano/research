aec18b88f75d4b9f65a8f5833edc9be09dc2ec51
David A.D. Morano <david.a.morano@gmail.com>
2026-01-09 00:52:50 -0500

    update

--- src/optiflow/debug.cc
+++ src/optiflow/debug.cc
@@ -1,212 +1,401 @@
-/* debug */
+/* debug SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
-/* debugging support */
+/* debugging stubs */
+/* version %I% last-modified %G% */
 
+#define	CF_DEBUGS	0		/* compile-time debugging */
 
-#define	CF_DEBUGS	1		/* debug print-outs */
+/* revision history:
 
+	= 1998-01-15, David Morano
+	This was written to debug the REXEC program.
 
-/* Copyright © 2003-2007 David A­D­ Morano.  All rights reserved. */
+*/
 
-/******************************************************************************
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
-	Debugging support subroutines.
+/*******************************************************************************
 
+  	Group:
+	debug
 
-******************************************************************************/
+	Description:
+	This modeule provides debugging support for the REXEC program.
 
+*******************************************************************************/
 
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>
-#include <cstdlib>
-#include <cstring>
+#include	<sys/param.h>
+#include	<sys/stat.h>
+#include	<unistd.h>
+#include	<fcntl.h>
+#include	<csignal>
+#include	<stropts.h>
+#include	<cstdlib>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>		/* |getenv(3c)| */
+#include	<cstring>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<uclibmem.h>
+#include	<estrings.h>
+#include	<storebuf.h>
+#include	<localmisc.h>
 
-/* some of Dave's libraries */
+#include	"debug.h"
 
-#include	<bio.h>
-#include	<sbuf.h>
-#include	<mallocstuff.h>
-#include	<paramfile.h>
+/* local defines */
 
-/* simple scalar stuff */
-
-#include "host.h"
-#include "misc.h"
-#include "machine.h"
-#include "regs.h"
-#include "memory.h"
-#include "loader.h"
-#include "syscall.h"
-#include "dlite.h"
-#include "options.h"
-#include "stats.h"
-#include "sim.h"
-
-/* from 'main' */
-
-#include	"localmisc.h"
-#include	"ssconfig.h"
-#include	"defs.h"
-
-#include	"ss.h"
-#include	"ssinfo.h"
-#include	"ssdecode.h"
-#include	"ssiw.h"
-#include	"ssas.h"
+#define	PRINTBUFLEN	(COLUMNS + 2)
+#define	HEXBUFLEN	100
 
 
+/* external subroutines */
 
-
-
-/* parse out into string form the flags of an instruction */
-int instr_flags(f,buf,buflen)
-uint	f ;
-char	buf[] ;
-int	buflen ;
-{
-	SBUF	b ;
-
-	int	rs ;
-
-
-	rs = sbuf_init(&b,buf,buflen) ;
-
-	if (rs < 0)
-		goto ret0 ;
-
-	if ((f & F_ICOMP) || (f & F_FCOMP)) {
-
-	    sbuf_strw(&b," COMP",-1) ;
-
-	    if (f & F_ICOMP)
-	        sbuf_strw(&b,"-INT",-1) ;
-
-	    if (f & F_FCOMP)
-	        sbuf_strw(&b,"-FLOAT",-1) ;
-
-	}
-
-	if (f & F_CTRL) {
-
-	    sbuf_strw(&b," CONTROL",-1) ;
-
-	    if (f & F_COND)
-	        sbuf_strw(&b,"-COND",-1) ;
-
-	    if (f & F_UNCOND)
-	        sbuf_strw(&b,"-UNCOND",-1) ;
-
-	}
-
-	if (f & F_MEM) {
-
-	    sbuf_strw(&b," MEMORY",-1) ;
-
-	    if (f & F_LOAD)
-	        sbuf_strw(&b,"-LOAD",-1) ;
-
-	    if (f & F_STORE)
-	        sbuf_strw(&b,"-STORE",-1) ;
-
-	    if (f & F_DISP)
-	        sbuf_strw(&b,"-DISP",-1) ;
-
-	    if (f & F_RR)
-	        sbuf_strw(&b,"-RR",-1) ;
-
-	    if (f & F_DIRECT)
-	        sbuf_strw(&b,"-DIRECT",-1) ;
-
-	}
-
-	if (f & F_TRAP) {
-
-	    sbuf_strw(&b," TRAP",-1) ;
-
-	}
-
-	if (f & F_LONGLAT) {
-
-	    sbuf_strw(&b," LONGLATENCY",-1) ;
-
-	}
-
-	if (f & F_DIRJMP)
-	    sbuf_strw(&b," DIRJMP",-1) ;
-
-	if (f & F_INDIRJMP)
-	    sbuf_strw(&b," INDIRJMP",-1) ;
-
-	if (f & F_CALL)
-	    sbuf_strw(&b," CALL",-1) ;
-
-	if (f & F_FPCOND)
-	    sbuf_strw(&b," FPCOND",-1) ;
-
-	if (f & F_IMM)
-	    sbuf_strw(&b," IMM",-1) ;
-
-	rs = sbuf_free(&b) ;
-
-ret0:
-	return rs ;
+extenr "C" {
+    extern int	bufprintf(char *,int,cchar *,...) noex ;
+    extern int	nprintf(cchar *,cchar *,...) noex ;
+    extern int	debugprintf(cchar *,...) noex ;
+    extern int	debugprint(cchar *,int) noex ;
 }
-/* end subroutine (instr_display) */
 
 
-/* dump an AS */
-int ss_asdump(mip,name,asp)
-SS		*mip ;
-const char	name[] ;
-SSAS		*asp ;
-{
-	OPERAND	*op ;
-
-	int	rs1, i ;
-
-	char	ibuf[100] ;
+/* external variables */
 
 
-	eprintf("ss_asdump: %s ia=%016llx\n",
-	    name,asp->n.ia) ;
+/* local structures */
 
-		rs1 = md_disassemble(asp->n.instrword,asp->n.ia,ibuf,99) ;
+struct debug_oflags {
+	int	m ;
+	char	*s ;
+} ;
 
-	eprintf("ss_asdump: instr> %w\n",ibuf,((rs1 > 0) ? rs1 : 0)) ;
 
-	eprintf("ss_asdump: op=%d class=%d flags=%06x\n",
-	    asp->n.op,asp->n.class,asp->n.flags) ;
+/* forward references */
 
-	eprintf("ss_asdump: nio=%d noo=%d\n",
-	    asp->n.nopsi,asp->n.nopso) ;
+int		mkhexstr(char *,int,cvoid *,int) noex ;
 
-	for (i = 0 ; i < asp->n.nopsi ; i += 1) {
+local int	checkbasebounds(cchar *,int,void *) noex ;
 
-	    op = &asp->n.opsi[i] ;
-	    eprintf("ss_asdump: IOP i=%d t=%u a=%016llx pv=%016llx\n",
-	        i,op->type,op->a,op->pv) ;
 
-	    if (op->type == OPERAND_TMEM)
-	        eprintf("ss_asdump: 	size=%d dp=%02x\n",
-	            op->size,op->dp) ;
+/* external variables */
 
+
+/* local variables */
+
+constexpr char	hextable[] = {
+	'0', '1', '2', '3', '4', '5', '6', '7',
+	'8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+} ;
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+char *d_reventstr(int revents,char *buf,int buflen) noex {
+
+#if	CF_DEBUGS
+	debugprintf("d_reventstr: ent\n") ;
+#endif
+
+	buf[0] = '\0' ;
+	bufprintf(buf,buflen,"%s %s %s %s %s %s %s %s %s",
+	    (revents & POLLIN) ? "I " : "  ",
+	    (revents & POLLRDNORM) ? "IN" : "  ",
+	    (revents & POLLRDBAND) ? "IB" : "  ",
+	    (revents & POLLPRI) ? "PR" : "  ",
+	    (revents & POLLWRNORM) ? "WN" : "  ",
+	    (revents & POLLWRBAND) ? "WB" : "  ",
+	    (revents & POLLERR) ? "ER" : "  ",
+	    (revents & POLLHUP) ? "HU" : "  ",
+	    (revents & POLLNVAL) ? "NV" : "  ") ;
+
+#if	CF_DEBUGS
+	debugprintf("d_reventstr: %s\n",buf) ;
+#endif
+
+	return buf ;
+}
+/* end subroutine (d_reventstr) */
+
+/* who is open? */
+void d_whoopen(cchar *s) noex {
+	int	rs = SR_OK ;
+
+	if (s) {
+	    debugprintf("d_whoopen: %s\n",s) ;
+	}
+	for (int i = 0 ; i < 20 ; i += 1) {
+	    if ((rs = u_fcntl(i,F_GETFL,0)) >= 0) {
+	        debugprintf("d_whoopen: open on %d accmod=%08x\n",
+	            i,(rs & O_ACCMODE)) ;
+	    }
 	} /* end for */
 
-	for (i = 0 ; i < asp->n.nopso ; i += 1) {
+}
+/* end subroutine (d_whoopen) */
 
-	    op = &asp->n.opso[i] ;
-	    eprintf("ss_asdump: OOP i=%d t=%u a=%016llx pv=%016llx v=%016llx\n",
-	        i,op->type,op->a,op->pv,op->v) ;
-
-	    if (op->type == OPERAND_TMEM)
-	        eprintf("ss_asdump: 	size=%d dp=%02x\n",
-	            op->size,op->dp) ;
-
+/* return a count of the number of open files */
+int d_openfiles() noex {
+	int	count = 0 ;
+	for (int i = 0 ; i < 2048 ; i += 1) {
+	    if (ustat sb ; (u_fstat(i,&sb) >= 0)) {
+	        count += 1 ;
+	    }
 	} /* end for */
+	return count ;
+}
+/* end subroutine (d_openfiles) */
 
+int d_ispath(cchar *p) noex {
+	if (p == NULL)
+	    return FALSE ;
 
+#ifdef	DEBFILE
+	nprintf(DEBFILE,"d_ispath: PATH=>%W<\n",
+	    p,strnlen(p,30)) ;
+#endif
+
+	return ((*p == '/') || (*p == ':')) ;
+}
+/* end subroutine (d_ispath) */
+
+int gdb() noex {
 	return 0 ;
 }
-/* end subroutine (ss_asdump) */
+/* end subroutine (gdb) */
 
+int mkhexstr(char *dbuf,int dlen,cvoid *vp,int vl) noex {
+	int	sl = vl ;
+	int	ch ;
+	int	j = 0 ; /* used-afterwards */
+	const uchar	*sp = (const uchar *) vp ;
+
+	if (sl < 0) sl = strlen(sp) ;
+
+	for (int i = 0 ; (dlen >= 3) && (i < sl) ; i += 1) {
+	    ch = sp[i] ;
+
+	    if (i > 0) dbuf[j++] = ' ' ;
+	    dbuf[j++] = hextable[(ch>>4)&15] ;
+	    dbuf[j++] = hextable[(ch>>0)&15] ;
+
+	    dlen -= ((i > 0) ? 3 : 2) ;
+	} /* end for */
+	dbuf[j] = '\0' ;
+
+	return j ;
+}
+/* end subroutine (mkhexstr) */
+
+int mkhexnstr(char *hbuf,int hlen,int maxcols,cchar *sbuf,int slen) noex {
+	int	n = 0 ;
+
+	if (maxcols < 0) maxcols = COLUMNS ;
+
+	if (slen < 0) slen = strlen(sbuf) ;
+	n = MIN((maxcols / 3),slen) ;
+	mkhexstr(hbuf,hlen,sbuf,n) ;
+
+	return n ;
+}
+/* end subroutine (mkhexnstr) */
+
+int debugprinthex(char *ids,int maxcols,cchar *sp,int sl) noex {
+	int	rs ;
+	int	idlen = 0 ;
+	int	wlen = 0 ;
+
+	char	printbuf[PRINTBUFLEN + 1] ;
+
+	if (ids != NULL) idlen = strlen(ids) ;
+
+	if (maxcols < 0) maxcols = COLUMNS ;
+
+	if (idlen > 0) maxcols -= (idlen + 1) ;
+
+	rs = mkhexnstr(printbuf,PRINTBUFLEN,maxcols,sp,sl) ;
+
+	if (rs >= 0) {
+	    if (idlen > 0) {
+	        rs = debugprintf("%t %s\n",ids,idlen,printbuf) ;
+	    } else {
+	        rs = debugprintf("%s\n",printbuf) ;
+	    }
+	    wlen = rs ;
+	} /* end if (ok) */
+
+	return (rs >= 0) ? wlen : rs ;
+}
+/* end subroutine (debugprinthex) */
+
+int debugprinthexblock(cchar *ids,int maxcols,cvoid *vp,int vl) noex {
+	int	rs = SR_OK ;
+	int	idlen = 0 ;
+	int	n ;
+	int	pbl ;
+	int	cslen ;
+	int	cols ;
+	int	len ;
+	int	sl = vl ;
+	int	wlen = 0 ;
+
+	cchar	*sp = (cchar *) vp ;
+
+	char	printbuf[PRINTBUFLEN + 1] ;
+	char	*pbp ;
+
+
+	if (ids != NULL) idlen = strlen(ids) ;
+
+	if (maxcols < 0) maxcols = COLUMNS ;
+
+	if (sl < 0) sl = strlen(sp) ;
+
+	i = 0 ;
+	while ((rs >= 0) && (sl > 0)) {
+
+	    pbp = printbuf ;
+	    pbl = PRINTBUFLEN ;
+	    cols = maxcols ;
+
+	    if (ids != NULL) {
+	        if ((idlen+2) < pbl) {
+	            int i = strwcpy(pbp,ids,idlen) - pbp ;
+		    pbp[i++] = ':' ;
+		    pbp[i++] = ' ' ;
+		    pbp += i ;
+		    pbl -= i ;
+		    cols -= i ;
+		} else {
+		    rs = SR_OVERFLOW ;
+		}
+	    } /* end if (non-null) */
+
+	    if (rs >= 0) {
+	        n = (cols / 3) ;
+	            cslen = MIN(n,sl) ;
+	            rs = mkhexstr(pbp,pbl,sp,cslen) ;
+	            sp += cslen ;
+	            sl -= cslen ;
+	    }
+
+	    if (rs >= 0) {
+	        len = debugprint(printbuf,-1) ;
+	        wlen += len ;
+	    }
+
+	} /* end while */
+
+	return (rs >= 0) ? wlen : rs ;
+}
+/* end subroutine (debugprinthexblock) */
+
+int hexblock(cchar *ids,cchar *ap,int n) noex {
+	int	sl ;
+	char	hexbuf[HEXBUFLEN + 3] ;
+
+	if (ids != NULL)
+	    debugprint(ids,-1) ;
+
+	for (int i = 0 ; i < n ; i += 1) {
+
+	    sl = mkhexstr(hexbuf,HEXBUFLEN,ap,4) ;
+
+	    hexbuf[sl++] = '\n' ;
+	    hexbuf[sl] = '\0' ;
+
+	    ap += 4 ;
+
+	    debugprint(hexbuf,-1) ;
+
+	} /* end for */
+
+	return n ;
+}
+/* end subroutine (hexblock) */
+
+/* audit a HOSTENT structure */
+int heaudit(hostent *hep,cchar *buf,int buflen) noex {
+	int	rs = SR_OK ;
+	char	**cpp ;
+
+	if (hep == NULL)
+	    return SR_FAULT ;
+
+	if (buf == NULL)
+	    return SR_FAULT ;
+
+	if (buflen < 0)
+	    return SR_INVALID ;
+
+	if (rs >= 0) {
+	    rs = checkbasebounds(buf,buflen,hep->h_name) ;
+	}
+
+	if (rs >= 0) {
+	    cpp = hep->h_aliases ;
+	    if (cpp != NULL) {
+	        rs = checkbasebounds(buf,buflen,cpp) ;
+	        if (rs >= 0) {
+	            for (int i = 0 ; cpp[i] != NULL ; i += 1) {
+	                rs = checkbasebounds(buf,buflen,(cpp + i)) ;
+	                if (rs >= 0) {
+	                    rs = checkbasebounds(buf,buflen,cpp[i]) ;
+			}
+	                if (rs < 0) break ;
+	            } /* end for */
+	        }
+	    }
+	}
+
+	if (rs >= 0) {
+	    cpp = hep->h_aliases ;
+	    if (cpp != NULL) {
+	        rs = checkbasebounds(buf,buflen,cpp) ;
+
+	        if (rs >= 0) {
+	            for (int i = 0 ; cpp[i] != NULL ; i += 1) {
+	                rs = checkbasebounds(buf,buflen,(cpp + i)) ;
+	                if (rs >= 0) {
+	                    rs = checkbasebounds(buf,buflen,cpp[i]) ;
+			}
+	                if (rs < 0) break ;
+	            } /* end for */
+	        }
+	    }
+	}
+
+	return rs ;
+}
+/* end subroutine (heaudit) */
+
+char *stroflags(char *buf,int oflags) noex {
+	int	rs = snflagsopen(buf,TIMEBUFLEN,oflags) ;
+	return (rs >= 0) ? buf : NULL ;
+}
+/* end subroutine (stroflags) */
+
+
+/* local subroutines */
+
+local int checkbasebounds(cchar *bbuf,int blen,void *vp) noex {
+	int	rs = SR_OK ;
+	cchar	*tp = (cchar *) vp ;
+
+	if ((rs >= 0) && (tp < bbuf))
+	    rs = SR_BADFMT ;
+
+	if ((rs >= 0) && (tp >= (bbuf + blen)))
+	    rs = SR_BADFMT ;
+
+	return rs ;
+}
+/* end subroutine (checkbasebounds) */
 
 
