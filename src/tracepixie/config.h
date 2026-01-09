f8b6138dfaf09595d71c4c403da2b06961ba59cf
David A.D. Morano <david.a.morano@gmail.com>
2026-01-09 00:51:36 -0500

    restore

---
+++ src/tracepixie/config.h
@@ -0,0 +1,24 @@
+/* config */
+
+
+#define	VERSION		"0a"
+#define	WHATINFO	"@(#)TRACEPIXIE "
+
+#define	VARPROGRAMROOT1	"TRACEPIXIE_PROGRAMROOT"
+#define	VARPROGRAMROOT2	"LOCAL"
+#define	VARPROGRAMROOT3	"PROGRAMROOT"
+
+#ifndef	PROGRAMROOT
+#define	PROGRAMROOT	"/usr/add-on/local"
+#endif
+
+#define	SEARCHNAME	"tracepixie"
+
+#define	ENVFNAME	"env"
+
+
+#define	VARDEBUGFD1	"TRACEPIXIE_DEBUGFD"
+#define	VARDEBUGFD2	"DEBUGFD"
+
+
+
