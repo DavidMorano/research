9b4729e158ca5124d3618e6ee593407cf44bc588
David A.D. Morano <david.a.morano@gmail.com>
2026-01-14 00:17:30 -0500

    restore

--- src/levosim/frame/main.cc
+++ src/levosim/frame/main.cc
@@ -49,7 +49,7 @@
 #include	<netdb.h>
 #include	<unistd.h>
 #include	<fcntl.h>
-#include	<time.h>
+#include	<ctime>
 #include	<cstdlib>
 #include	<cstring>
 #include	<cerrno>		/* for signal catching */
