6448a425e43856089530f774028635a8bc1e94ed
David A.D. Morano <david.a.morano@gmail.com>
2026-02-24 00:16:26 -0500

    restore

---
+++ src/tracedbx/config.h
@@ -0,0 +1,24 @@
+/* config */
+
+
+#define	VERSION		"1"
+#define	WHATINFO	"@(#)TRACEDBX "
+
+#define	VARPROGRAMROOT1	"TRACEDBX_PROGRAMROOT"
+#define	VARPROGRAMROOT2	"LOCAL"
+#define	VARPROGRAMROOT3	"PROGRAMROOT"
+
+#ifndef	PROGRAMROOT
+#define	PROGRAMROOT	"/usr/add-on/local"
+#endif
+
+#define	SEARCHNAME	"tracedbx"
+
+#define	ENVFNAME	"env"
+
+
+#define	VARDEBUGFD1	"TRACEDBX_DEBUGFD"
+#define	VARDEBUGFD2	"DEBUGFD"
+
+
+
