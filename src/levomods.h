b135d82e928b370dce461a20163f317c9505ae9c
David A.D. Morano <david.a.morano@gmail.com>
2026-03-01 00:56:24 -0500

    restore

---
+++ src/levomods.h
@@ -0,0 +1,41 @@
+/* levomods HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* Levo modules */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2000-03-15, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	LEVOMODS_INCLUDE
+#define	LEVOMODS_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+#include	<levomod.h>
+#include	<sshdb.h>
+
+#include	<bpalpha.h>
+#include	<bpbimode.h>
+#include	<bpeveight.h>
+#include	<bpgskew.h>
+#include	<bpgspag.h>
+#include	<bptourna.h>
+#include	<bpvpred.h>
+#include	<bpyags.h>
+
+
+#endif /* LEVOMODS_INCLUDE */
+
+
