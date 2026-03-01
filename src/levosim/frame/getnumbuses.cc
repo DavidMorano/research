3e7f46790a8bdeea0c96353b52a47f358f962347
David A.D. Morano <david.a.morano@gmail.com>
2026-03-01 14:08:44 -0500

    update

--- src/levosim/frame/getnumbuses.cc
+++ src/levosim/frame/getnumbuses.cc
@@ -1,53 +1,52 @@
-/* getnumbuses */
+/* getnumbuses SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
 
 /* calculate the number of buses from an interleave schedule */
-
+/* version %I% last-modified %G% */
 
 #define	CF_DEBUGS	0		/* non-switchable */
 
+/* revision history:
 
-/* revision history :
-
-	= 00/02/15, Dave Morano
-
+	= 2000-02-15, Dave Morano
 	This code was started.
-
 
 */
 
-
-
+/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
 
 /******************************************************************************
 
+  	Name:
+	getnumbuses
+
+	Description:
 	This subroutine calculates the number of buses (or memory banks)
 	given an interleave schedule for that set of buses (or memory
 	block).
 
-
 ******************************************************************************/
 
-
-
-
-#include	<sys/types.h>
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<cstdlib>
+#include	<cstddef>		/* |nullptr_t| */
 #include	<cstring>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<localmisc.h>
 
-#include	<usystem.h>
-
-#include	"localmisc.h"
 #include	"defs.h"
-
 
 
 /* local defines */
 
 
-
 /* external subroutines */
 
-extern int	fbscounti(uint), ffbsi(uint), flbsi(uint) ;
+
+/* external variables */
 
 
 /* local structures */
@@ -56,21 +55,21 @@ extern int	fbscounti(uint), ffbsi(uint), flbsi(uint) ;
 /* forward references */
 
 
+/* local variables */
 
 
+/* exported variables */
 
-/* calculate the number of buses from an interleave schedule */
-int getnumbuses(inter)
-uint	inter ;
-{
+
+/* exported subroutines */
+
+int getnumbuses(int inter) {
 	int	n ;
-
-
-	n = fbscounti(inter) ;
-
+	{
+	    n = fbscounti(inter) ;
+	}
 	return (1 << n) ;		/* n = 2 ^ n */
 }
 /* end subroutine (getnumbuses) */
-
 
 
