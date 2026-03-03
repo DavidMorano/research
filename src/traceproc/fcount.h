346b606983c8145959edef4fc5934a31fda1213a
David A.D. Morano <david.a.morano@gmail.com>
2026-03-03 00:10:50 -0500

    update

--- src/traceproc/fcount.h
+++ src/traceproc/fcount.h
@@ -1,76 +1,68 @@
-/* fcount */
+/* fcount HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 
 /* revision history:
 
 	= 1998-11-01, David Morano
-
 	Originally written for Audix Database Processor (DBP) work.
-
 
 */
 
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 #ifndef	FCOUNT_INCLUDE
-#define	FCOUNT_INCLUDE	1
+#define	FCOUNT_INCLUDE
 
 
-#include	<envstandards.h>
-
-#include	<sys/types.h>
-#include	<time.h>
-
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
 #include	<vecitem.h>
-#include	<localmisc.h>
 
 #include	"lmapprog.h"
 
 
-/* object defines */
-
-#define	FCOUNT			struct fcount_head
-#define	FCOUNT_ENTRY		struct fcount_entry
+#define	FCOUNT		struct fcount_head
+#define	FCOUNT_ENT	struct fcount_entry
+#define	FCOUNT_MAGIC	0x23456787
+#define	FCOUNT_DEFFUNCS	300		/* default entries */
 
 
 struct fcount_entry {
-	char	*name ;				/* name of function */
-	uint	ia ;				/* instruction address */
-	uint	size ;				/* size of function */
-	uint	calls ;				/* calls */
-	uint	ins ;				/* instructions executed */
-} ;
+	char		*name ;		/* name of function */
+	uint		ia ;		/* instruction address */
+	uint		size ;		/* size of function */
+	uint		calls ;		/* calls */
+	uint		ins ;		/* instructions executed */
+} ; /* end struct */
 
 struct fcount_head {
-	unsigned long		magic ;
-	vecitem			table ;
-	uint			in ;		/* instruction count */
-	uint			func_i ;	/* index */
-	uint			func_ia, func_size ;
-	uint			other_calls, other_ins ;
-} ;
+	vecitem		table ;
+	uint		magic ;
+	uint		in ;		/* instruction count */
+	uint		func_i ;	/* index */
+	uint		func_ia, func_size ;
+	uint		other_calls, other_ins ;
+} ; /* end struct */
 
+typedef	FCOUNT		fcount ;
+typedef	FCOUNT_ENT	fcount_ent ;
 
-#if	(! defined(FCOUNT_MASTER)) || (FCOUNT_MASTER == 0)
+EXTERNC_begin
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
+extern int	fcount_init(fcount *,lmapprog *,cchar *) noex ;
+extern int	fcount_update(fcount *,uint,int) noex ;
+extern int	fcount_done(fcount *) noex ;
+extern int	fcount_sort(fcount *,int (*)()) noex ;
+extern int	fcount_get(fcount *,int,fcount_ent **) noex ;
+extern int	fcount_gettotal(fcount *,uint *,uint *) noex ;
+extern int	fcount_getother(fcount *,uint *,uint *) noex ;
+extern int	fcount_free(fcount *) noex ;
 
-extern int	fcount_init(FCOUNT *,LMAPPROG *,const char *) ;
-extern int	fcount_update(FCOUNT *,uint,int) ;
-extern int	fcount_done(FCOUNT *) ;
-extern int	fcount_sort(FCOUNT *,int (*)()) ;
-extern int	fcount_get(FCOUNT *,int,FCOUNT_ENTRY **) ;
-extern int	fcount_gettotal(FCOUNT *,uint *,uint *) ;
-extern int	fcount_getother(FCOUNT *,uint *,uint *) ;
-extern int	fcount_free(FCOUNT *) ;
+EXTERNC_end
 
-#ifdef	__cplusplus
-}
-#endif
-
-#endif /* FCOUNT_MASTER */
 
 #endif /* FCOUNT_INCLUDE */
 
