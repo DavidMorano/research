db1b9c0e69e0e46114530e4303f4de1a3a06674a
David A.D. Morano <david.a.morano@gmail.com>
2026-03-03 00:11:06 -0500

    update

--- src/traceproc/dataval.h
+++ src/traceproc/dataval.h
@@ -1,16 +1,20 @@
-/* dataval */
+/* dataval HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 
 /* revision history:
 
 	= 1998-11-01, David Morano
-
 	Originally written for Audix Database Processor (DBP) work.
-
 
 */
 
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
 
 
 #ifndef	DATAVAL
