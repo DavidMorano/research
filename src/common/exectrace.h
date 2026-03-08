940d8ce96ea0657c394c7ca55ddc096de1f44af7
David A.D. Morano <david.a.morano@gmail.com>
2026-03-08 00:26:50 -0500

    update

--- src/common/exectrace.h
+++ src/common/exectrace.h
@@ -24,16 +24,15 @@
 #include	<sys/param.h>
 #include	<netdb.h>		/* for MAXHOSTNAMELEN */
 #include	<bfile.h>
-#include	<localmisc.h>		/* for 'ulong' */
 
 
 /* object defines */
-#define	EXECTRACE_MAGIC		0x86553823
 #define	EXECTRACE		struct exectrace_head
-#define	EXECTRACE_INFO		struct exectrace_info
-#define	EXECTRACE_ENTRY		struct exectrace_e
-#define	EXECTRACE_OPERAND	struct exectrace_operand
+#define	EXECTRACE_INFO		struct exectrace_infomation
+#define	EXECTRACE_ENT		struct exectrace_entry
+#define	EXECTRACE_OPER		struct exectrace_operand
 /* other defines */
+#define	EXECTRACE_MAGIC		0x86553823
 #define	EXECTRACE_FILEMAGIC	"EXECTRACE"
 #define	EXECTRACE_FILEVERSION	0
 #define	EXECTRACE_NAMELEN	MAXNAMELEN
@@ -72,22 +71,22 @@ struct exectrace_reading {
 	ulong	clock ;			/* running clock value (reading) */
 	ulong	in ;			/* instruction number */
 	int	type ;			/* last type (reading) */
-} ;
+} ; /* end struct */
 
-struct exectrace_info {
+struct exectrace_infomation {
 	ulong	in ;			/* instruction number */
 	long	date ;
 	int	version ;		/* file version */
 	char	name[EXECTRACE_NAMELEN + 1] ;
 	char	where[EXECTRACE_WHERELEN + 1] ;
-} ;
+} ; /* end struct */
 
 struct exectrace_flags {
 	uint	eof : 1 ;		/* EOF indication on reading */
 	uint	in : 1 ;		/* got a starting instruction number */
 	uint	read : 1 ;		/* reading or writing */
 	uint	append : 1 ;		/* appending */
-} ;
+} ; /* end struct */
 
 struct exectrace_head {
 	uint		magic ;
@@ -100,13 +99,13 @@ struct exectrace_head {
 	char		istypeinfo[EXECTRACE_ROVERLAST] ;
 	char		istypesom[EXECTRACE_ROVERLAST] ;
 	char		istypeextra[EXECTRACE_ROVERLAST] ;
-} ;
+} ; /* end struct */
 
 struct exectrace_operand {
 	uint	a ;			/* address */
 	uint	dv ;			/* value */
 	uint	dp ;			/* data present bits */
-} ;
+} ; /* end struct */
 
 struct exectrace_eflags {
 	uint	clock : 1 ;
@@ -120,37 +119,42 @@ struct exectrace_eflags {
 	uint	smem : 8 ;		/* number of source memory variables */
 } ;
 
-struct exectrace_e {
-	struct exectrace_eflags		f ;
+struct exectrace_entry {
 	ulong				clock ;
 	ulong				in ;
 	struct exectrace_operand	reg[EXECTRACE_NREG] ;
 	struct exectrace_operand	sreg[EXECTRACE_NREG] ;
 	struct exectrace_operand	mem[EXECTRACE_NMEM] ;
 	struct exectrace_operand	smem[EXECTRACE_NMEM] ;
+	struct exectrace_eflags		f ;
 	uint				ia ;
 	uint				som ;
 	uint				sc ;
-} ;
+} ; /* end struct */
+
+typedef	EXECTRACE		exectrace ;
+typedef	EXECTRACE_INFO		exectrace_info ;
+typedef	EXECTRACE_ENT		exectrace_ent ;
+typedef	EXECTRACE_OPER		exectrace_oper ;
 
 EXTERNC_begin
 
-extern int	exectrace_open(EXECTRACE *,cchar *,
-			cchar *,int, cchar *) ;
-extern int	exectrace_close(EXECTRACE *) ;
-extern int	exectrace_wclock(EXECTRACE *,ulong) ;
-extern int	exectrace_win(EXECTRACE *,ulong) ;
-extern int	exectrace_wia(EXECTRACE *,uint) ;
-extern int	exectrace_wsom(EXECTRACE *,uint) ;
-extern int	exectrace_wsyscall(EXECTRACE *,uint) ;
-extern int	exectrace_wreg(EXECTRACE *,uint,uint,uint) ;
-extern int	exectrace_wrsa(EXECTRACE *,uint) ;
-extern int	exectrace_wrsv(EXECTRACE *,uint,uint) ;
-extern int	exectrace_wmem(EXECTRACE *,uint,uint,uint) ;
-extern int	exectrace_wmsa(EXECTRACE *,uint) ;
-extern int	exectrace_wmsv(EXECTRACE *,uint,uint) ;
-extern int	exectrace_read(EXECTRACE *,EXECTRACE_ENTRY *) ;
-extern int	exectrace_flush(EXECTRACE *) ;
+extern int	exectrace_open(exectrace *,cchar *,
+			cchar *,int, cchar *) noex ;
+extern int	exectrace_close(exectrace *) noex ;
+extern int	exectrace_wclock(exectrace *,ulong) noex ;
+extern int	exectrace_win(exectrace *,ulong) noex ;
+extern int	exectrace_wia(exectrace *,uint) noex ;
+extern int	exectrace_wsom(exectrace *,uint) noex ;
+extern int	exectrace_wsyscall(exectrace *,uint) noex ;
+extern int	exectrace_wreg(exectrace *,uint,uint,uint) noex ;
+extern int	exectrace_wrsa(exectrace *,uint) noex ;
+extern int	exectrace_wrsv(exectrace *,uint,uint) noex ;
+extern int	exectrace_wmem(exectrace *,uint,uint,uint) noex ;
+extern int	exectrace_wmsa(exectrace *,uint) noex ;
+extern int	exectrace_wmsv(exectrace *,uint,uint) noex ;
+extern int	exectrace_read(exectrace *,evecstrace_ent *) noex ;
+extern int	exectrace_flush(exectrace *) noex ;
 
 EXTERNC_end
 
