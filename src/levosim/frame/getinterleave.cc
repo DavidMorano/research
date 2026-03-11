aef397d14cc6ce66f09aed14a308b5ab063e7bc3
David A.D. Morano <david.a.morano@gmail.com>
2026-03-11 00:14:21 -0400

    update

--- src/levosim/frame/getinterleave.cc
+++ src/levosim/frame/getinterleave.cc
@@ -1,94 +1,77 @@
-/* getinterleave */
+/* getinterleave SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 /* get a memory interleave index from an address and interleave schedule */
-
+/* version %I% last-modified %G% */
 
 #define	CF_DEBUGS	0		/* non-switchable debug print-outs */
 
+/* revision history:
 
-/* revision history :
-
-	= 00/07/18, Dave Morano
-
+	= 2000-07-18, Dave Morano
 	This subroutine was written to calculate the proper bus
 	to use when interleaved buses are provided.
 
-
 */
 
+/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
 
 /******************************************************************************
 
-	This subroutine takes an interleave schedule and an address and
-	calculates which interleaved bus (by bus index) should be used
-	for for the associated operation given the address.
+  	Name:
 
+	Description:
+	This subroutine takes an interleave schedule and an address
+	and calculates which interleaved bus (by bus index) should
+	be used for for the associated operation given the address.
 
-	Synopsis :
+	Synopsis:
+	int getinterleave(uint interleave,uint addr)
 
-	int getinterleave(interleave,addr)
-	uint	interleave, addr ;
-
-
-	Arguments :
-
+	Arguments:
 	+ interleave	the interleave schedule
 	+ address	the address that is in view
 
-
-	Returns :
-
+	Returns:
 	index		the bus index of the interleaved buses
-
-
 
 ******************************************************************************/
 
-
-
-#include	<sys/types.h>
-
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<cstddef>
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<usysbase.h>
 #include	<bitops.h>
+#include	<findbit.h>
+#include	<localmisc.h>
 
-#include	"localmisc.h"
 
+/* local defines */
 
 
 /* external subroutines */
 
-extern int	ffbsi(uint) ;
+
+/* exported variables */
 
 
+/* exported subroutines */
 
-
-
-
-int getinterleave(interleave,addr)
-uint	interleave, addr ;
-{
+int getinterleave(uint interleave,uint addr) noex {
 	int	result = 0 ;
-	int	i ;
-	int	bn ;
-
-
-	i = 0 ;
-	bn = ffbsi(interleave) ;
-
-	while (bn >= 0) {
-
-	    if (BTSTI(&addr,bn))
+	for (int i = 0, bn = ffbs(interleave) ; bn >= 0 ; ) {
+	    if (BTSTI(&addr,bn)) {
 	        result |= (1 << i) ;
-
+	    }
 	    BCLRI(&interleave,bn) ;
-
 	    i += 1 ;
 	    bn = ffbsi(interleave) ;
-
 	} /* end while */
-
 	return result ;
 }
 /* end subroutine (getinterleave) */
-
 
 
